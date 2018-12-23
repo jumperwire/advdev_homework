2b93ad5c9426f545ba846aff52141ab50aad16f8
jumperwire <hendrasutanto@gmail.com>
2018-12-24 01:22:57 +0800

    Update setup_nexus.sh

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -29,3 +29,7 @@ echo "Setting up Nexus in project $GUID-nexus"
 # oc new-app -f ../templates/nexus.yaml --param .....
 
 # To be Implemented by Student
+
+oc new-project $GUID-nexus --display-name "Shared Nexus"
+
+oc process -f ../templates/nexus.yaml | oc create -f - -n $GUID-nexus
