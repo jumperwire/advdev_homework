b84d1842d5186b3ed8ba6f4b91e1083b896272ec
jumperwire <hendrasutanto@gmail.com>
2018-12-25 01:33:28 +0800

    Update setup_jenkins.sh

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -36,4 +36,8 @@ oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi
 
 oc new-build  -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
       USER root\nRUN yum -y install skopeo && yum clean all\n
-      USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
+      USER 1001' --name=jenkins-slave-appdev -n ${GUID}-jenkins
+
+oc new-build ${REPO}#master --name=mlbparks-pipeline --context-dir=./MLBParks -e GUID=${GUID} -e CLUSTER=${CLUSTER} -n ${GUID}-jenkins
+oc new-build ${REPO}#master --name=nationalparks-pipeline --context-dir=./Nationalparks -e GUID=${GUID} -e CLUSTER=${CLUSTER} -n  ${GUID}-jenkins
+oc new-build ${REPO}#master --name=parksmap-pipeline --context-dir=./ParksMap -e GUID=${GUID} -e CLUSTER=${CLUSTER} -n  ${GUID}-jenkins
