64ec8ac282f8ea6ae0ca1c05a7d09cdbd21ba7b6
jumperwire <hendrasutanto@gmail.com>
2018-12-25 20:07:05 +0800

    Update setup_prod.sh

--- Infrastructure/bin/setup_prod.sh
+++ Infrastructure/bin/setup_prod.sh
@@ -13,3 +13,12 @@ echo "Setting up Parks Production Environment in project ${GUID}-parks-prod"
 # The Green services/routes need to be active initially to guarantee a successful grading pipeline run.
 
 # To be Implemented by Student
+oc project ${GUID}-parks-prod
+
+oc policy add-role-to-group system:image-puller system:serviceaccounts:4d09-parks-prod -n 4d09-parks-dev
+
+oc policy add-role-to-user edit system:serviceaccount:4d09-jenkins:jenkins -n 4d09-parks-prod
+
+oc policy add-role-to-user view --serviceaccount=default -n 4d09-parks-prod
+
+oc process -f ./Infrastructure/templates/parks-prod.yaml | oc create -f - -n ${GUID}-parks-prod
